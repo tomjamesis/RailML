b04c2f9f69204b275280f4031875941b05e894ea
Tom James <52748031+TomJamesAtDataSummit@users.noreply.github.com>
2020-11-18 13:43:56 +0000

    API project added
    API project added without specific controllers

--- v3.1/UI/Server/Controllers/WeatherForecastController.cs
+++ v3.1/UI/Server/Controllers/WeatherForecastController.cs
@@ -25,10 +25,10 @@ namespace UI.Server.Controllers
         }
 
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        public IEnumerable<SampleTable> Get()
         {
             var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            return Enumerable.Range(1, 5).Select(index => new SampleTable
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = rng.Next(-20, 55),
